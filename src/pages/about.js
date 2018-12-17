b3120efd8fd4cb697ab6f3a2f8951d122194781b
Anh Le <anh.le@beteasy.com>
2018-12-17 11:19:50 +1100

    feat: Adding page and link.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="About header" />
-    <p>About page content.</p>
-  </div>
+    <div style={{ color: `teal` }}>
+        <Link to="/">Home</Link>
+        <Header headerText="About header" />
+        <p>About page content.</p>
+    </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from '../components/header'
 
 export default () => (
     <div style={{ color: `purple` }}>
+        <Link to="/about">About</Link> _/\_/\_/\_ 
+        <Link to="/contact">Contact</Link>
         <Header headerText="Index header" />
         <div>Index page content.</div>
     </div>
