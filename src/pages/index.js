08fe3738af00ace1b79495599c4032c1758bfc0a
Anh Le <anh.le@beteasy.com>
2018-12-17 11:11:34 +1100

    feat: Adding basic component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import Header from '../components/header'
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `purple` }}>
+        <Header headerText="Index header" />
+        <div>Index page content.</div>
+    </div>
+)
