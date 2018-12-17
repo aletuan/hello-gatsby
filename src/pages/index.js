dbadcffbf780febb8d9ca6813406e017a668f684
Anh Le <anh.le@beteasy.com>
2018-12-17 13:12:02 +1100

    feat: Adding font plugin and font configure

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,23 @@ import { Link } from "gatsby"
 import Header from '../components/header'
 
 export default () => (
-    <div style={{ color: `purple` }}>
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    {" "}
         <Link to="/about">About</Link> _/\_/\_/\_ 
         <Link to="/contact">Contact</Link>
         <Header headerText="Index header" />
-        <div>Index page content.</div>
+        <div>
+            <p>
+                This is dummy content in the index.
+            </p>
+            <blockquote>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    <em>
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </em>
+                </p>
+            </blockquote>
+        </div>
     </div>
 )
