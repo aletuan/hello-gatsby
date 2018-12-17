21909a4cc389d9d7f4039c7ee0d6f11462b1aaa7
Anh Le <anh.le@beteasy.com>
2018-12-17 13:19:24 +1100

    doc: Adding font type comment

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,8 @@ import Typography from "typography"
 import funstonTheme from 'typography-theme-funston'
 funstonTheme.baseFontSize = '22px' // was 20px.
 
+// Refer more font and settings
+// http://kyleamathews.github.io/typography.js/
 const typography = new Typography(funstonTheme)
 
 export default typography
