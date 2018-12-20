4c446e8f41b080731d656945df51f76c886f9d22
Anh Le <anh.le@beteasy.com>
2018-12-20 13:15:44 +1100

    fix: Link warning issue

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -11,16 +11,12 @@ import 'prismjs/themes/prism-twilight.css'
 import './index.scss'
 
 const TemplateWrapper = ({ children, data }) => {
-  let user
-  if (typeof window !== 'undefined') {
-    user = window.netlifyIdentity && window.netlifyIdentity.currentUser()
-  }
   return (
     <StaticQuery query={pageQuery} render={data => (
       <div className='App'>
         <Helmet title={data.site.siteMetadata.title} />
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand" href="#">{data.site.siteMetadata.title}</a>
+                <a class="navbar-brand" href="/index">{data.site.siteMetadata.title}</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
