8945ddc75c841bf86fc9e8d17bfa37c745f566ff
Anh Le <anh.le@beteasy.com>
2018-12-20 14:53:05 +1100

    fix: Small bug

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -14,10 +14,10 @@ const TemplateWrapper = ({ children, data }) => {
     <StaticQuery query={pageQuery} render={data => (
       <div className='App'>
         <Helmet title={data.site.siteMetadata.title} />
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a class="navbar-brand" href="/index">{data.site.siteMetadata.title}</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand" href="/index">{data.site.siteMetadata.title}</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
             </nav>
         <div className='pageContent'>{children}</div>
@@ -27,7 +27,7 @@ const TemplateWrapper = ({ children, data }) => {
 }
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func
+  children: PropTypes.object,
 }
 
 const pageQuery = graphql`
