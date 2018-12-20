ae104270aa5b45743d828e984791d145bfca9614
Anh Le <anh.le@beteasy.com>
2018-12-20 16:43:01 +1100

    fix: Update navigation bar

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import GBNavbar from '../navbar'
 // code syntax-highlighting theme
 // feel free to change it to another one
 import 'prismjs/themes/prism-twilight.css'
@@ -14,30 +15,7 @@ const TemplateWrapper = ({ children, data }) => {
     <StaticQuery query={pageQuery} render={data => (
       <div className='App'>
         <Helmet title={data.site.siteMetadata.title} />
-
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
+        <GBNavbar />
         <div className='pageContent'>{children}</div>
       </div>
     )} />
