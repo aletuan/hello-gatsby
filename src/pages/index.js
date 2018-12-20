e7db31a3dd92440a6ed994b9686af5c19acc1c0c
Anh Le <anh.le@beteasy.com>
2018-12-20 22:05:56 +1100

    feat: Remove jumbotron in home

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,24 +10,22 @@ import Layout from '../components/layout';
 export default () => (
     <Layout>
         <Container>
-            <Jumbotron>
-                <p className="lead">
-                    Good day, <FaBeer size={30} style={{ color: 'black' }} />
-                </p>
-                <p>
-                    Thanks you for the visiting. This is my personal website. 
-                    It is a memo what I learn from software development, and other non-technical stuffs.
-                    However, I also hope you could find some userful information from here.
-                    If you have any feedback, drop me a message or send me an email to <a href="mailto:anhlt1983@gmail.com">anhlt1983@gmail.com</a>
-                </p>
-                <p>
-                    <em>
-                        The best preparation for tomorrow is doing your best today.
-                    </em>
-                    <br />
-                    <b style={{ float: `right` }}>H. Jackson Brown, Jr.</b>                                   
-                </p>
-            </Jumbotron>
+            <p className="lead">
+                Good day, <FaBeer size={30} style={{ color: 'black' }} />
+            </p>
+            <p>
+                Thanks you for visiting. This is my personal website. 
+                It memo what I learn from software development, and other non-technical stuffs.
+                I also hope you could find some userful information from here.
+                If you have any feedback, send me an email to <a href="mailto:anhlt1983@gmail.com">anhlt1983@gmail.com</a>
+            </p>
+            <p>
+                <em>
+                    The best preparation for tomorrow is doing your best today.
+                </em>
+                <br />
+                <b style={{ float: `right` }}>H. Jackson Brown, Jr.</b>                                   
+            </p>
         </Container>
     </Layout>
 )
