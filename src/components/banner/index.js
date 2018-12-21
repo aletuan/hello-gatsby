93acb0083e6de992673e54725ebf62c94b9ec191
Anh Le <anh.le@beteasy.com>
2018-12-21 17:25:12 +1100

    refactor: Create separated banner component

---
+++ src/components/banner/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+    Container,
+    Card,
+    CardBody,
+} from 'reactstrap';
+
+const Banner = () => (
+    <Container>
+        <Card>
+            <CardBody>                  
+                <p>
+                    Thanks you for visiting. This is my personal website to memo what I learn.
+                    I also hope you could find some userful information from here.
+                    If you have any feedback, send me an email to <a href="mailto:anhlt1983@gmail.com">anhlt1983@gmail.com</a>
+                </p>
+                <p>
+                    <em>
+                        The best preparation for tomorrow is doing your best today.
+                    </em>
+                    <br />
+                    <b style={{ float: `right` }}>H. Jackson Brown, Jr.</b>                                   
+                </p>
+            </CardBody>
+        </Card>
+    </Container>    
+)
+
+export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,10 @@
 import React from "react"
-import {
-    Container,
-    Card,
-    CardBody,
-} from 'reactstrap';
 
 import Layout from '../components/layout';
+import Banner from '../components/banner';
 
 export default () => (
     <Layout>
-        <Container>
-            <Card>
-                <CardBody>                  
-                    <p>
-                        Thanks you for visiting. This is my personal website to memo what I learn.
-                        I also hope you could find some userful information from here.
-                        If you have any feedback, send me an email to <a href="mailto:anhlt1983@gmail.com">anhlt1983@gmail.com</a>
-                    </p>
-                    <p>
-                        <em>
-                            The best preparation for tomorrow is doing your best today.
-                        </em>
-                        <br />
-                        <b style={{ float: `right` }}>H. Jackson Brown, Jr.</b>                                   
-                    </p>
-                </CardBody>
-            </Card>
-        </Container>
+        <Banner />
     </Layout>
 )
