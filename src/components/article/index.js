53804741042f910645d869823cbbd864d7169eb0
Anh Le <anh.le@beteasy.com>
2018-12-22 17:32:35 +1100

    feat: Decorate feed

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -6,32 +6,42 @@ import {
     CardBody,
     CardHeader,
     CardFooter,
+    CardTitle,
+    Row,
+    Col,
 } from "reactstrap"
 
 // TODO: Playing button, featuring (author),  meta (discuss, tag)
 const Toolbar = () => (
     <CardFooter>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">Toolbar</a>
     </CardFooter>
 )
 
-const Article =  ({ title, description }) => {
-    return (                   
-        <Card style={{ margin: "20px"}}>
-            <CardHeader>
-                {title}
-            </CardHeader>
-            <CardBody>
-                {description}                
-            </CardBody>
-            <Toolbar />
-        </Card>                       
+const Article =  ({ title, description, imgUrl }) => {
+    return (
+        <Row style={{ marginTop: "10px" }}>
+            <Col>
+                <Card>
+                    <CardBody>
+                        <p style={{ alignItems: "center", display: "flex", margin: "0 0 10px" }}>
+                            <img src={imgUrl} style={{ margin: "2px 10px 10px 0px", borderRadius: "3px" }} with="50" height="50" />
+                            <a style={{ textDecoration: "none", color: "#101820" }} href="https://www.github.com">github.com</a>
+                        </p>
+                        <h2 style={{ fontSize: "1.25rem", lineHeight: "1.25rem", margin: "10px 0"}} >{title}</h2>
+                        {description}
+                    </CardBody>
+                    <Toolbar />
+                </Card>
+            </Col>
+        </Row>                                       
     );   
 }
 
 Article.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
+    imgUrl: PropTypes.string,
 };
 
 export default Article;
--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -7,9 +7,17 @@ import Article from  '../article';
 
 export default () => {
     return (                   
-        <Container style={{ marginTop: "20px" }}>
-            <Article title="Something" description="Inside" />                      
-            <Article title="Beautiful" description="Outside" />                      
+        <Container>
+            <Article 
+                title="Jumping off the Edge into Chromium" 
+                description="Nick, KBall, and Chris respond to follow up on the State of JavaScript survey, discuss Chromium, Edge, and the future of the web, and reminisce about the past year in the final JS Party" 
+                imgUrl="https://cdn.changelog.com/uploads/icons/topics/jq0/icon_small.png"
+            />                      
+            <Article 
+                title="Brave's new BAT tipping banner for creators" 
+                description="Good news! The new Brave tipping banner is now available with the latest desktop browser update. This new banner makes the experience of tipping your favorite Brave-verified" 
+                imgUrl="https://cdn.changelog.com/uploads/icons/topics/4Q/icon_small.png"
+            />                      
         </Container>
     );   
 }
